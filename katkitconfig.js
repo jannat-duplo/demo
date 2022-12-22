29730ba25bcf140800631cdc657e95f9fa943ce1
jannat-duplo <jannat@duplocloud.net>
2022-12-22 16:26:13 +0530

    testing katkit

---
+++ katkitconfig.js
@@ -0,0 +1,24 @@
+[
+    {
+        "EnvName": "default",
+        "LocalFleet": "true",
+        "WorkFlow": [
+            {
+                "Name": "PRE_DEPLOY_BUILD",
+                "PhaseType": 4,
+                "BuildParams": "PHASE=PRE_DEPLOY_BUILD, FOO=BAR",
+                "Order": 0,
+                "Parallelism": 1,
+                "ContinerImage": "nginx:latest"
+            },
+            {
+                "Name": "DEPLOY",
+                "PhaseType": 1,
+                "BuildParams": "PHASE=DEPLOY",
+                "Order": 1,
+                "Parallelism": 1,
+                "ContinerImage": null
+            }
+        ]
+    }
+]
